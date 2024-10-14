bf3966cd933f4d25b31058874d2b3ce5772ebd39
Preston TW <preston_tunnell_wilson@brown.edu>
2024-10-13 23:51:18 -0400

    Initial commit
    setting up nix and rustup

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils, ... }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = with pkgs; mkShell {
+          buildInputs = [
+            pnpm
+            rustup
+            tailwindcss
+            just
+            watchexec
+          ];
+        };
+      });
+}
+
+
