1ae279f9cbb309f0ab678b4655101a0ef7747c5f
Preston TW <preston_tunnell_wilson@brown.edu>
2024-10-27 10:01:13 -0400

    Migrate to InertiaJS v2 beta
    Fixes missing type declarations in svelte files and
    corrects some typings in main.ts

--- web-client/src/main.ts
+++ web-client/src/main.ts
@@ -1,14 +1,14 @@
 import './app.css'
-import { createInertiaApp } from '@inertiajs/svelte'
+import { createInertiaApp, type ResolvedComponent } from '@inertiajs/svelte'
 import { mount } from 'svelte'
 
 createInertiaApp({
-  resolve: (name: string) => {
-    const pages = import.meta.glob('./Pages/**/*.svelte', { eager: true })
+  resolve: name => {
+    const pages: {[page: string]: ResolvedComponent} = import.meta.glob('./Pages/**/*.svelte', { eager: true })
     return pages[`./Pages/${name}.svelte`]
   },
   setup({ el, App, props }) {
-    mount(App, { target: el, props })
+    mount(App, { target: el!, props })
   },
 })
 
