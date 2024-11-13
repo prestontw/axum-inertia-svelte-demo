f833e65e09cede0d613dadff3631aa3b899faea4
Preston TW <preston_tunnell_wilson@brown.edu>
2024-11-13 12:39:56 -0500

    Update test to create new user
    instead of relying on existing one

--- web-client/e2e/demo.test.ts
+++ web-client/e2e/demo.test.ts
@@ -1,15 +1,26 @@
 import { expect, test } from '@playwright/test';
 
-test('update titles', async ({ page }) => {
+test('create new user and edit', async ({ page }) => {
   await page.goto('http://localhost:3000/');
+  await page.getByRole('button', { name: 'Add User' }).click();
+  await page.getByRole('textbox').click();
+  await page.getByRole('textbox').fill('New Person');
+  await page.getByRole('button', { name: 'Add Title' }).click();
+  await page.getByRole('textbox').nth(1).click();
+  await page.getByRole('textbox').nth(1).fill('Sr.');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await expect(page.getByRole('article')).toContainText('Hello, there New Person (Sr.)');
+
   await page
     .locator('#app div')
-    .filter({ hasText: 'Hello, there John Smith (Mr' })
+    .filter({ hasText: 'Hello, there New Person (Sr' })
     .getByRole('button')
     .click();
+  await page.getByRole('textbox').nth(1).click();
+  await page.getByRole('textbox').nth(1).fill('Jr.');
   await page.getByRole('button', { name: 'Add Title' }).click();
-  await page.locator('input:nth-child(3)').click();
-  await page.locator('input:nth-child(3)').fill('Sr.');
+  await page.getByRole('textbox').nth(2).click();
+  await page.getByRole('textbox').nth(2).fill('Esq.');
   await page.getByRole('button', { name: 'Submit' }).click();
-  await expect(page.getByRole('article')).toContainText('Sr.');
+  await expect(page.getByRole('article')).toContainText('Hello, there New Person (Jr. Esq.)');
 });
