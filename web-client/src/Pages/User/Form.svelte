ec547120ee6b599553733966fb00166f5ab59335
Preston TW <preston_tunnell_wilson@brown.edu>
2024-12-26 15:43:29 -0500

    Revert "Try to use form helper"
    This reverts commit 85987b6fb48d60937f97e419f60288b6954d67f7.

--- web-client/src/Pages/User/Form.svelte
+++ web-client/src/Pages/User/Form.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { useForm } from '@inertiajs/svelte';
+  import { router } from '@inertiajs/svelte';
   import type { User } from '../../shared_definitions';
   import { userIdRoute, usersRoute } from '../../routes';
 
@@ -10,36 +10,36 @@
     onsubmit,
   }: { name?: string; titles?: string[]; id?: number; onsubmit?: () => void } = $props();
 
-  const form = useForm({ name: originalName ?? '', titles: originalTitles ?? [], id });
+  let name = $state(originalName ?? '');
+  let titles = $state(originalTitles ?? []);
 
   function submit(e) {
     e.preventDefault();
-
+    let form: Partial<User> = { name, titles, id };
     if (id) {
-      $form.put(userIdRoute({ id }));
+      router.put(userIdRoute({ id }), form);
     } else {
-      $form.post(usersRoute({}));
+      router.post(usersRoute({}), form);
     }
     onsubmit?.();
   }
-  $inspect($form);
 </script>
 
 <form onsubmit={submit}>
-  <input type="text" bind:value={$form.name} />
+  <input type="text" bind:value={name} />
   <!--
    {#if form.errors.email}
   <div class="form-error">{form.errors.email}</div>
   {/if}
     -->
-  {#each $form.titles as _, i}
-    <input type="text" bind:value={$form.titles[i]} />
+  {#each titles as _, i}
+    <input type="text" bind:value={titles[i]} />
   {/each}
   <button
     class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
     onclick={(e) => {
       e.preventDefault();
-      $form.titles.push('');
+      titles.push('');
     }}>Add Title</button
   >
   <button type="submit">Submit</button>
