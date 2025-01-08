80a5979221e0ca921a97954c311a3cbf453cc2ba
Preston TW <preston_tunnell_wilson@brown.edu>
2025-01-08 11:26:30 -0500

    Get titles working through `useForm`
    Took the classic "reassign to spread array" js trick, but it works now

--- web-client/src/Pages/User/Form.svelte
+++ web-client/src/Pages/User/Form.svelte
@@ -1,38 +1,38 @@
 <script lang="ts">
-  import { router } from '@inertiajs/svelte';
-  import type { NewUser, User } from '../../shared_definitions';
+  import { useForm } from '@inertiajs/svelte';
+  import type { User } from '../../shared_definitions';
   import { userIdRoute, usersRoute } from '../../routes';
 
   let { user: originalUser, onsubmit }: { user?: User; onsubmit?: () => void } = $props();
 
-  let user: User | NewUser = $state(originalUser ?? { name: '', titles: [] });
+  let form = useForm(originalUser ? { ...originalUser } : { name: '', titles: [] });
 
   function submit(e) {
     e.preventDefault();
-    if ('id' in user) {
-      router.put(userIdRoute({ id: user.id }), user);
+    if ('id' in $form) {
+      $form.put(userIdRoute({ id: $form.id }));
     } else {
-      router.post(usersRoute({}), user);
+      $form.post(usersRoute({}));
     }
     onsubmit?.();
   }
 </script>
 
 <form onsubmit={submit}>
-  <input type="text" bind:value={user.name} />
+  <input type="text" bind:value={$form.name} />
   <!--
    {#if form.errors.email}
   <div class="form-error">{form.errors.email}</div>
   {/if}
     -->
-  {#each user?.titles as _, i}
-    <input type="text" bind:value={user.titles[i]} />
+  {#each $form.titles as _, i}
+    <input type="text" bind:value={$form.titles[i]} />
   {/each}
   <button
     class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
     onclick={(e) => {
       e.preventDefault();
-      user.titles.push('');
+      $form.titles = [...$form.titles, ''];
     }}>Add Title</button
   >
   <button type="submit">Submit</button>
